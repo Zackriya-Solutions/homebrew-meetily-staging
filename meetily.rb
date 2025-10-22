1794ce030a296d520e437a663f078cc292641eb5
athulchandroth <chandroth@zackriya.com>
2025-10-22 20:26:43 +0530

    Automated test: Deploy old version

--- meetily.rb
+++ meetily.rb
@@ -1,16 +1,16 @@
 cask "meetily" do
-  version "0.0.6"
-  # sha256 "fa5708fed0a9f03db524f26a2190dfd72a636e731414416710a7f63b05466aa6"
-  sha256 "bfe69737fabb03fa21c35a5e3665fafc8dc463a1a885f6bac6f5dac633409d15"
-  url "https://github.com/Zackriya-Solutions/meeting-minutes/releases/download/test-v0.0.6/dmg_test_0.0.6.zip"
+  version "0.0.5"
+  sha256 "2bac406c82c7a4545c12e1650daef585983704873c0483e932ec56f907dcb08f"
+  url "https://github.com/Zackriya-Solutions/meeting-minutes/releases/download/v0.0.5/dmg_darwin_arch64_0.0.5.zip"
   name "Meetily"
   desc "Meeting transcription and analysis application"
   homepage "https://github.com/Zackriya-Solutions/meeting-minutes"
 
+  depends_on formula: "zackriya-solutions/meetily-staging/meetily-backend"
   depends_on macos: ">= :monterey"
   depends_on arch: :arm64
 
-  container nested: "dmg/meetily-frontend_0.0.6_aarch64.dmg"
+  container nested: "dmg/meetily-frontend_0.0.5_aarch64.dmg"
   
   app "meetily-frontend.app"
   
@@ -30,10 +30,13 @@ cask "meetily" do
 
   caveats do
     <<~EOS
-      Meetily now includes an integrated backend server!
+      Meetily requires the backend server to be running.
       
-      Simply launch the application - no separate backend setup required.
-      The integrated backend will start automatically with the frontend.
+      To start the backend server:
+        meetily-server
+      
+      The frontend will automatically connect to the backend at:
+        http://localhost:5167
     EOS
   end
 end
